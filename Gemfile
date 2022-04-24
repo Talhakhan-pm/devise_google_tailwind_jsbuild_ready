2b9274fa0cc80e88f85679c352cdb51db5fa2fa8
Khan <Talhaakhan@pm.me>
2022-04-24 04:53:55 -0700

    devise ready with google oauth2

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "devise", "~> 4.8"
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'static_pages/dashboard'
   get 'static_pages/landing_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
